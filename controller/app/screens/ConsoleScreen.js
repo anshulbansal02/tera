2d3da3c7d4703022eacff18df6628fa630f7b4b0
anshulbansal02 <anshulbansal02@outlook.com>
2020-06-04 19:10:35 +0530

    Added custom Switch component in controller components

--- controller/app/screens/ConsoleScreen.js
+++ controller/app/screens/ConsoleScreen.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Joystick from '../shared/components/Joystick';
+import Switch from '../shared/components/Switch';
 
 
 export default class ConsoleScreen extends Component {
@@ -8,6 +9,8 @@ export default class ConsoleScreen extends Component {
   render() {
     return (
       <>
+      {/* <Switch onChange={(value) => console.log(value)}></Switch>
+      <Switch onChange={(value) => console.log(value)}></Switch> */}
         <Joystick></Joystick>
         </>
     );
---
+++ controller/app/shared/components/Switch.js
@@ -0,0 +1,99 @@
+import React, { Component } from "react";
+import {
+	View,
+	StyleSheet,
+	Animated,
+	TouchableWithoutFeedback,
+	Easing,
+} from "react-native";
+
+export default class Switch extends Component {
+	constructor(props) {
+		super(props);
+
+		this.anim_pos = new Animated.Value(5);
+		this.anim_color = new Animated.Value(0);
+
+		this.state = {
+			value: false,
+		};
+	}
+
+	// Animated properties: thumb = postion, color; track: backgroundColor
+
+	toggle = () => {
+		if (this.state.value) {
+			Animated.timing(this.anim_pos, {
+				toValue: 5,
+				duration: 200,
+				easing: Easing.inOut(Easing.quad),
+            }).start();
+            Animated.timing(this.anim_color, {
+                toValue: 0,
+				duration: 200,
+				easing: Easing.inOut(Easing.quad),
+            }).start();
+		} else {
+			Animated.timing(this.anim_pos, {
+				toValue: 30,
+				duration: 200,
+				easing: Easing.inOut(Easing.quad),
+            }).start();
+            Animated.timing(this.anim_color, {
+                toValue: 1,
+				duration: 200,
+				easing: Easing.inOut(Easing.quad),
+            }).start();
+		}
+
+		this.props.onChange(!this.state.value);
+		this.setState({ value: !this.state.value });
+	};
+
+	render() {
+		const trackBGColor = this.anim_color.interpolate({
+			inputRange: [0, 1],
+			outputRange: ["rgba(0,0,0,0.15)", "rgba(0, 133, 254, 1)"],
+		});
+
+		return (
+			<TouchableWithoutFeedback
+				onPress={this.toggle}
+				accessibilityRole="switch"
+			>
+				<Animated.View style={[style.track, {backgroundColor: trackBGColor}]}>
+					<Animated.View
+						style={[style.thumb, { left: this.anim_pos }]}
+					></Animated.View>
+				</Animated.View>
+			</TouchableWithoutFeedback>
+		);
+	}
+}
+
+const style = StyleSheet.create({
+	thumb: {
+		width: 25,
+		height: 25,
+		backgroundColor: "#fff",
+		borderRadius: 100,
+		elevation: 5,
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 1,
+		},
+		shadowOpacity: 0.22,
+		shadowRadius: 2.22,
+	},
+
+	track: {
+		top: 200,
+		left: 100,
+		width: 60,
+		height: 35,
+		backgroundColor: "rgba(0,0,0,0.15)",
+		borderRadius: 100,
+		justifyContent: "center",
+	},
+});
