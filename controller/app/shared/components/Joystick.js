13a94454915559dbe3a27742ea9ece93722eeb46
anshulbansal02 <anshulbansal02@outlook.com>
2020-06-06 17:03:45 +0530

    Refactored Joystick component to use react-native-reanimated

--- controller/app/shared/components/Joystick.js
+++ controller/app/shared/components/Joystick.js
@@ -1,92 +1,82 @@
 import React, { Component } from "react";
-import { View, StyleSheet, Easing, Text, Animated } from "react-native";
+import { View, StyleSheet } from "react-native";
 import { PanGestureHandler, State } from "react-native-gesture-handler";
 
-export default class Joystick extends Component {
+import Animated, {
+	block,
+	set,
+	cond,
+	eq,
+	call,
+	add,
+	sqrt,
+	multiply,
+	pow,
+	Value,
+	event,
+	min,
+	cos,
+	sin,
+} from "react-native-reanimated";
+import { atan2 } from "react-native-redash";
+
+export default class Joy extends Component {
 	constructor(props) {
 		super(props);
 
-		this.anim_x = new Animated.Value(0);
-		this.anim_y = new Animated.Value(0);
+		this.distance = new Value(0);
+		this.angle = new Value(0);
+		this.touchState = new Value(-1);
+		this.transX = new Value(0);
+		this.transY = new Value(0);
 
 		this.maxOffset = 70;
 
-		this.state = {
-			xTransform: 0,
-			yTransform: 0,
-		};
-	}
-
-	componentDidUpdate(prevProps, prevState) {
-		const prevXY = prevState;
-		const currXY = this.state;
-
-		// Move to utils, Remember to iterpolate values between 0-100
-		Object.keys(prevXY).map(function (key, index) {
-			prevXY[key] = Math.round(prevXY[key]);
-		});
-
-		Object.keys(currXY).map(function (key, index) {
-			currXY[key] = Math.round(currXY[key]);
-		});
-
-		if (
-			!(JSON.stringify(prevXY) === JSON.stringify(currXY)) &&
-			this.props.onValue
-		)
-			this.props.onValue(currXY);
-	}
-
-	centerAnimatedPostion = () => {
-		Animated.timing(this.anim_x, {
-			toValue: this.state.xTransform,
-			duration: 200,
-			easing: Easing.elastic(),
-		}).start();
-		Animated.timing(this.anim_y, {
-			toValue: this.state.yTransform,
-			duration: 200,
-			easing: Easing.elastic(),
-		}).start();
-	};
-
-	setAnimatedPosition = () => {
-		Animated.timing(this.anim_x, {
-			toValue: this.state.xTransform,
-			duration: 0,
-		}).start();
-		Animated.timing(this.anim_y, {
-			toValue: this.state.yTransform,
-			duration: 0,
-		}).start();
-	};
-
-	handleDrag = (event) => {
-		const xOffset = event.nativeEvent.translationX;
-		const yOffset = event.nativeEvent.translationY;
-
-		const angle = Math.atan2(yOffset, xOffset);
-		const distance = Math.min(this.maxOffset, Math.hypot(xOffset, yOffset));
-
-		const xTransform = distance * Math.cos(angle);
-		const yTransform = distance * Math.sin(angle);
-
-		this.setState(
+		this.handleDrag = event([
 			{
-				xTransform,
-				yTransform,
+				nativeEvent: ({ translationX, translationY, state }) =>
+					block([
+						set(this.touchState, state),
+						set(
+							this.distance,
+							min(
+								this.maxOffset,
+								sqrt(
+									add(
+										pow(translationX, 2),
+										pow(translationY, 2)
+									)
+								)
+							)
+						),
+						set(
+							this.angle,
+							min(
+								this.maxOffset,
+								atan2(translationY, translationX)
+							)
+						),
+						set(
+							this.transX,
+							multiply(this.distance, cos(this.angle))
+						),
+						set(
+							this.transY,
+							multiply(this.distance, sin(this.angle))
+						),
+						call([this.transX, this.transY], this.onValue),
+					]),
 			},
-			this.setAnimatedPosition
-		);
-	};
+		]);
 
-	handleLeave = (event) => {
-		if ((event.nativeEvent.state = State.END)) {
-			this.setState(
-				{ xTransform: 0, yTransform: 0 },
-				this.centerAnimatedPostion
-			);
-		}
+		this.anim_x = cond(eq(this.touchState, State.ACTIVE), this.transX);
+
+		this.anim_y = cond(eq(this.touchState, State.ACTIVE), this.transY);
+	}
+
+	onValue = ([x, y]) => {
+		// console.log(x,y)
+		this.props.onValue ? this.props.onValue(x,y) : null;
 	};
 
 	render() {
@@ -95,7 +85,7 @@ export default class Joystick extends Component {
 				<View style={style.base}>
 					<PanGestureHandler
 						onGestureEvent={this.handleDrag}
-						onHandlerStateChange={this.handleLeave}
+						onHandlerStateChange={this.handleDrag}
 					>
 						<Animated.View
 							style={[
@@ -123,8 +113,11 @@ const style = StyleSheet.create({
 	joystick: {
 		width: 130,
 		height: 130,
+		top: 200,
+		left: 100,
 		alignItems: "center",
 		justifyContent: "center",
+		marginRight: 300,
 	},
 
 	base: {
